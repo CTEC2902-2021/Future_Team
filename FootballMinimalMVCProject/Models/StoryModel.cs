61c6f6cc89895ef2b775c68c314aac891b872bf4
Jessica Dimaku <P2530640@my365.dmu.ac.uk>
2021-03-22 22:51:13 +0000

    Add project files.

---
+++ FootballMinimalMVCProject/Models/StoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FootballMinimalMVCProject.Models
+{
+    public class StoryModel
+    {
+        public string Headline { get; set; }
+        public string ByLine { get; set; }
+        public string Content { get; set; }
+    }
+}
