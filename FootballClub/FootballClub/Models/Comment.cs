c64324ab333fd6c1f396082ddf6a78d4411a0d2d
alloteyphil <alloteyphilip@gmail.com>
2021-05-13 11:11:59 +0100

    changes

---
+++ FootballClub/FootballClub/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FootballClub.Models
+{
+    public class Comment
+    {
+        public int id { get; set; }
+        public string content { get; set; }
+        public int parent { get; set; }
+    }
+}
